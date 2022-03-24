c12346d9226a30ff8e785086fd2ba06fbf8ac1f4
JONATHAN SU <jonasu9661328@gmail.com>
2022-03-24 10:29:30 -0600

    Add files via upload

---
+++ PeriodicTableProcessing/src/Element.java
@@ -0,0 +1,35 @@
+public class Element {
+    public Series series;
+    private String name, symbol;
+    private int atomicNumber;
+    private double weight;
+
+    public Element(Series series, String name, String symbol, int atomicNumber, double weight) {
+	this.series = series;
+	this.name = name;
+	this.symbol = symbol;
+	this.atomicNumber = atomicNumber;
+	this.weight = weight;
+
+    }
+
+    public Series getSeries() {
+        return series;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getAtomicNumber() {
+        return atomicNumber;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
---
+++ PeriodicTableProcessing/src/ElementComponent.java
@@ -0,0 +1,46 @@
+import processing.core.*;
+
+public class ElementComponent {
+    public static final int RECT_SIZE = 80, CORNER_RADIUS = 20;
+
+    private PApplet sketch;
+    private Element element;
+    private float x, y;
+
+    public ElementComponent(PApplet sketch, Element element, float x, float y) {
+	this.sketch = sketch;
+	this.element = element;
+	this.x = x;
+	this.y = y;
+    }
+
+    public void render() {
+	sketch.fill(element.series.color[0], element.series.color[1], element.series.color[2]);
+	sketch.stroke(240);
+	sketch.strokeWeight(2);
+	sketch.rect(x, y, RECT_SIZE, RECT_SIZE, CORNER_RADIUS);
+
+	if (inBounds()) {
+	    sketch.fill(255, 0, 0);
+	} else {
+	    if (element.series.isMetal) {
+		sketch.fill(0);
+	    } else {
+		sketch.fill(255);
+	    }
+	}
+	sketch.textFont(PeriodicTable.fontNumber);
+	sketch.text(element.getAtomicNumber(), x + 10, y + 30);
+	sketch.textFont(PeriodicTable.fontSymbol);
+	sketch.text(element.getSymbol(), x + 10, y + 60);
+    }
+
+    public Element getElement() {
+	return element;
+    }
+
+    public boolean inBounds() {
+	return (sketch.mouseX > x) && (sketch.mouseY > y) && (sketch.mouseX < x + RECT_SIZE)
+		&& (sketch.mouseY < y + RECT_SIZE);
+    }
+}
