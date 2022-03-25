5a3e49187b646dba0c067125e960a15dbd38bb97
JONATHAN SU <jonasu9661328@gmail.com>
2022-03-25 11:28:32 -0600

    Delete PeriodicTableProcessing directory

--- PeriodicTableProcessing/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Element"},{"p":"<Unnamed>","l":"ElementComponent"},{"p":"<Unnamed>","l":"PeriodicTable"},{"p":"<Unnamed>","l":"Series"}];updateSearchResults();
